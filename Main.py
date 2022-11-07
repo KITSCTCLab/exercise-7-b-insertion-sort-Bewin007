e8430776c045be48f58ba932493921208a60abcc
Bewin Felix <99880789+Bewin007@users.noreply.github.com>
2022-11-07 18:13:23 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -15,7 +15,7 @@ def insertionSort(array) -> List[int]:
 # data = [9, 5, 1, 4, 3]
 input_data = input()
 data = []
-for item in input_data.split(', '):
+for item in input_data.split(','):
   if item.isnumeric():
     data.append(int(item))
   elif item.lstrip("-").isnumeric():
